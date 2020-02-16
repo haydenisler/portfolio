3b9067eab7af73b74a09179765b2b0ddbdb6479f
Hayden Isler <me@haydenisler.com>
2020-02-15 22:03:35 -0600

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div> Hi! </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data: () => {
+    return {
+      msg: 'Hello!'
     }
   }
 }
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=VT323&display=swap');
+
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+  background-image: linear-gradient(
+    #f9ad6a,
+    #ff7b89, 
+    #8a5082, 
+    #6f5f90, 
+    #205072, 
+    #329d9c, 
+    #56c596, 
+    #7be495
+  );
+  background-size: 100% 800%;
+  animation: background-animation 30s /*240s*/ linear infinite;
+
+  @keyframes background-animation {
+    0% {
+      background-position: 0% 0%;
+    }
+    50% {
+      background-position: 0% 100%;
+    }
+    100% {
+      background-position: 0% 0%;
+    }
+  }
+}
+
+
+
+#app {
+  font-family: 'VT323', monospace;
+  font-size: 2rem;
+  color: white;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: rgba(0, 0, 0, 0.15);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
 </style>
